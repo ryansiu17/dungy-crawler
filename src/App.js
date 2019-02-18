f7b9c3f3b508543e3d09569ca01fd88cf601df83
Ryan Alexander Siu <31353270+ryansiu17@users.noreply.github.com>
2019-02-17 18:29:38 -0800

    began work on screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.scss";
 import NavBar from "./NavBar";
+import ScreenTest from "./screens/ScreenTest";
 class App extends Component {
   state = {
     name: ""
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className="app">
         <div className="gameScreen">
+          <ScreenTest />
           <NavBar />
         </div>
       </div>
